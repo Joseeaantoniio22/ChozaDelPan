225a97cde33ee064784cfe89167334477c951fda
Joseeaantoniio22 <jfergue055@g.educaand.es>
2025-04-29 13:44:04 +0200

    avanzado

--- JS/index.js
+++ JS/index.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('.menu');      // Correcto: querySelector pa
 boton.addEventListener('click', () => {
     menu.classList.toggle('active'); // Activamos/desactivamos la clase
 });
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
